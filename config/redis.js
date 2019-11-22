4da0d7a24dd681b0c1f09869a83b77b535102df2
Julio Camargo <julio.camargo@gruponewway.com.br>
2019-11-22 09:16:06 -0300

    Improving env reading

--- config/redis.js
+++ config/redis.js
@@ -1,18 +1,11 @@
-module.exports = {
-  redisConnections: () => {
-    const envValue = process.env.KAIROS_REDIS_URL || [];
-    let arrayValue = [];
-    if (envValue.length > 0) {
-      arrayValue = envValue.split(', ') || [];
-    }
-    const response = [];
-    arrayValue.forEach((element) => {
-      const item = element.split(':');
-      response.push({
-        host: item[0],
-        port: Number(item[1]),
-      });
-    });
-    return response;
-  },
-};
+let redisCluster = [];
+
+if (process.env.KAIROS_REDIS_URL) {
+  redisCluster = process.env.KAIROS_REDIS_URL.trim().split(',').reduce((redisConfigs, redis) => {
+    const [host, port] = redis.split(':');
+    redisConfigs.push({ host, port: Number(port) });
+    return redisConfigs;
+  }, []);
+}
+
+module.exports = redisCluster;
